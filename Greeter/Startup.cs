407af3a4000b7bbb0b83393403a90061aeb466ba
kosalanuwan <kosala.nuwan@gmail.com>
2020-11-15 15:56:43 +0000

    refactor: remove unwanted code

--- Greeter/Startup.cs
+++ Greeter/Startup.cs
@@ -9,25 +9,5 @@ namespace Greeter
         {
             builder.Services.AddHttpClient();
         }
-
-        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
-        {
-            base.ConfigureAppConfiguration(builder);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
-        }
     }
 }
